dcccbe3fce96f72486b9b8b145bd9feb0f00a9a7
Eric Schmidt <e.ryan.schmidt@gmail.com>
2025-03-04 12:39:34 -0700

    Update console.log descriptions.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { client } from './lib/client.js';
+import { client } from "./lib/client.js";
 
 const response = await client.getEntries();
 
 // Log the Space ID of each entry to showcase resolved refs.
 response.includes.Entry.forEach((entry) => {
-  console.log(entry.sys.space.sys.id);
+  console.log("ADDITIONAL SPACE ID", entry.sys.space.sys.id);
 });
+
+console.log("ITEMS", response.items);
+console.log("INCLUDES", response.includes.Entry);
